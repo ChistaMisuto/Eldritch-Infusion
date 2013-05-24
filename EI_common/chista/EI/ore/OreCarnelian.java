27da4cfd5c22ebc718c65db05307982a20a74456
Somebody <pckuijper@gmail.com>
2013-05-24 22:48:04 +0200

    Fixed block textures

--- EI_common/chista/EI/ore/OreCarnelian.java
+++ EI_common/chista/EI/ore/OreCarnelian.java
@@ -3,18 +3,15 @@ package chista.EI.ore;
 import java.util.Random;
 
 import net.minecraft.block.material.Material;
+import chista.EI.EI;
 import chista.EI.item.ModItems;
 import chista.EI.lib.Strings;
 
 public class OreCarnelian extends ModOreBase {
 	public OreCarnelian(int id) {
 		super(id, Material.rock);
-<<<<<<< HEAD:EI_common/chista/EI/ore/OreCarnelia.java
-		this.setUnlocalizedName(Strings.CARNELIAORE_NAME);
-=======
 		this.setUnlocalizedName(Strings.CARNELIANORE_NAME);
-		this.setCreativeTab(CreativeTabs.tabBlock);
->>>>>>> ba0838478c83d69efb9321f213c5f739610c6a3d:EI_common/chista/EI/ore/OreCarnelian.java
+		this.setCreativeTab(EI.creativeTabEI);
 	}
 
 	@Override
